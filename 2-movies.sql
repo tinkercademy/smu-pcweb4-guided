68d6394fd8c089676b9f651bbd0811713f0eb8fe
YJ Soon <yjsoon@tinkertanker.com>
2020-09-04 12:20:02 +0800

    Updated to new 1-4

---
+++ 2-movies.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movies(
+    movie_id INT PRIMARY KEY AUTO_INCREMENT,
+    movie_name VARCHAR(30)
+);
+INSERT INTO movies VALUES(NULL,"movie1");
+INSERT INTO movies VALUES(NULL,"movie2");
+INSERT INTO movies VALUES(NULL,"movie3");
+INSERT INTO movies VALUES(NULL,"movie4");
+INSERT INTO movies VALUES(NULL,"movie5");
---
+++ 3-movie_reviews.sql
@@ -0,0 +1,14 @@
+CREATE TABLE movie_reviews(
+    user_id INT,
+    movie_id INT,
+    ratings DEC(2,1),
+    PRIMARY KEY(user_id,movie_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY(movie_id) REFERENCES movies(movie_id) ON DELETE CASCADE,
+    CHECK (ratings <= 5.0)
+);
+INSERT INTO reviews VALUES(1,1,4.5);
+INSERT INTO reviews VALUES(1,3,3.0);
+INSERT INTO reviews VALUES(2,1,4.7);
+INSERT INTO reviews VALUES(3,5,2.5);
+INSERT INTO reviews VALUES(4,4,4.1);
