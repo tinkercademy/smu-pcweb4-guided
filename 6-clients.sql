2ad28b341f89837715f07a82656d08bf9ece448c
YJ Soon <yjsoon@tinkertanker.com>
2021-08-17 22:09:32 +0800

    Rename employees SQL files

--- 6-clients.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE clients(
-client_id INT NOT NULL AUTO_INCREMENT,
-client_name VARCHAR(50),
-PRIMARY KEY (client_id)
-);
--- 7-works_with.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE works_with(
-emp_id INT NOT NULL,
-client_id INT NOT NULL,
-total_sales INT NOT NULL,
-PRIMARY KEY (emp_id, client_id), 
-FOREIGN KEY (emp_id) REFERENCES employees (emp_id) ON DELETE CASCADE,
-FOREIGN KEY (client_id) REFERENCES clients (client_id) ON DELETE CASCADE
-);
