b3d8af0b10d171f47e7c648de454c117432eb97e
YJ Soon <yjsoon@tinkertanker.com>
2021-08-19 11:09:05 +0800

    Changed followee to following

--- movie4-user_relation.sql
+++ movie4-user_relation.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user_relation(
     follower_id INT,
-    followee_id INT,
-    PRIMARY KEY(follower_id, followee_id),
+    following_id INT,
+    PRIMARY KEY(follower_id, following_id),
     FOREIGN KEY(follower_id) REFERENCES userinfo(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (followee_id) REFERENCES userinfo(user_id) ON DELETE CASCADE
+    FOREIGN KEY (following_id) REFERENCES userinfo(user_id) ON DELETE CASCADE
 );
 INSERT INTO user_relation VALUES(1,2);
 INSERT INTO user_relation VALUES(1,3);
