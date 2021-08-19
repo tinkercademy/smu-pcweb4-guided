1146220723507e28cc3d5219aca974a1bbb3ad42
YJ Soon <yjsoon@tinkertanker.com>
2021-08-19 12:41:51 +0800

    Remove ID from testdb

--- testdb.sql
+++ testdb.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS userinfo (
-  `id` int(9) unsigned NOT NULL,
   `username` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8;
+)
+
+
