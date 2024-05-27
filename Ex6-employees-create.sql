77c541faa2d3214b29d7b48947fce2b552b740e6
Sebastian Choo <sebaschoo04@gmail.com>
2024-05-27 21:53:15 +0800

    Update Ex6-employees-create.sql
    Moved primary key definition to be part of column creation

--- Ex6-employees-create.sql
+++ Ex6-employees-create.sql
@@ -1,8 +1,7 @@
 CREATE TABLE employees(
-    emp_id INT NOT NULL AUTO_INCREMENT,
+    emp_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
     salary INT,
     super_id INT,
-    PRIMARY KEY (emp_id), 
     FOREIGN KEY (super_id) REFERENCES employees (emp_id) ON DELETE SET NULL
 );
